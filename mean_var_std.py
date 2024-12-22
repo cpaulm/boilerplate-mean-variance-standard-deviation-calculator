3f4177c0854c3a0d1bb3b18131e0c25754ab63b9
cpaulm paul <manalangchristianpaul.learn@gmail.com>
2024-12-22 09:04:28 +0000

    Initial commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 def calculate(list):
+    """
+    Freecodecamp
+    """
 
-
-
-
-    return calculations
+#     calculations = {
+#   'mean': [axis1, axis2, flattened],
+#   'variance': [axis1, axis2, flattened],
+#   'standard deviation': [axis1, axis2, flattened],
+#   'max': [axis1, axis2, flattened],
+#   'min': [axis1, axis2, flattened],
+#   'sum': [axis1, axis2, flattened]
+# }
+    # return calculations
